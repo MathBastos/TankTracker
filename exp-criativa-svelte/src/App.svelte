8a77ccc0d5ec75f6d79348f251b819a3ef29da26
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-04-12 20:38:00 -0300

    Implementação de telas estáticas
    Telas: Registro aquário, registro usuário e login

---
+++ exp-criativa-svelte/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Login from "./components/telas/login/login.svelte";
+  import RegistroPessoa from "./components/telas/registroPessoa/registroPessoa.svelte";
+  import RegistroAquario from "./components/telas/registroAquario/registroAquario.svelte";
+</script>
+
+<Login/>
+<!-- <RegistroPessoa/> -->
+<!-- <RegistroAquario/> -->
